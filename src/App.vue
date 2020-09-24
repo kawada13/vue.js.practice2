1edb686bfaa4f6cc7a798339da6828896843460c
kawada <vgnsl94@gmail.com>
2020-09-24 19:27:13 +0900

    props学習

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
+    <hr>
+    <p>ブログの内容</p>
+    <hr>
+    <LikeNumber :age="age"></LikeNumber>
     <LikeNumber></LikeNumber>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./LikeHeader.vue";
+import LikeHeader from "./components/LikeHeader.vue";
 
 export default {
   components: {
     LikeHeader: LikeHeader,
   },
+  data: function() {
+    return {
+      age: 6
+    }
+  }
 };
 </script>
 
--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -2,11 +2,14 @@
   <div>
     <p>いいね({{number}})</p>
     <button @click="increment">+1</button>
+    <p>{{age}}</p>
+    <p>{{halfNumber}}</p>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['age'],
   data:function() {
     return {
       number: 32,
@@ -17,6 +20,11 @@ export default {
       this.number += 1;
     },
   },
+  computed: {
+    halfNumber: function() {
+      return this.age / 2;
+    }
+  }
 };
 </script>
 
